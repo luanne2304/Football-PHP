3cdd0803d2c836a37ca7ff7b019c060aecb3553d
luanne2304 <luannek2304@gmail.com>
2023-12-27 08:54:06 +0700

    first commit

---
+++ js/fp.js
@@ -0,0 +1,139 @@
+function addbuttonadmin(){
+  var a=document.getElementById("showitemadmin");
+  a.style.display="inline";
+}
+
+function addbuttonlogin(){
+  var a=document.getElementById("showlogin");
+  a.style.display="inline";
+}
+
+function addbuttonlogout(){
+  var a=document.getElementById("showlogout");
+  a.style.display="inline";
+}
+//modal
+var abc = document.getElementById("myModal");
+var btn = document.getElementById("nhapttct");
+// var btn2 = document.getElementById("update");
+// var def = document.getElementById("modalupdate");
+var close = document.getElementsByClassName("close")[0];
+// /*  tại sao lại có [0] như  thế này bởi vì mỗi close là một html colection nên khi mình muốn lấy giá trị html thì phải thêm [0]. 
+// Nếu mình có 2 cái component cùng class thì khi [0] nó sẽ hiển thị component 1 còn [1] thì nó sẽ hiển thị component 2. */
+var close_footer = document.getElementsByClassName("close-footer")[0];
+var order = document.getElementsByClassName("order")[0];
+
+
+// var close_footer1 = document.getElementsByClassName("close-footer")[1];
+// var order1 = document.getElementsByClassName("order")[1];
+// var close1 = document.getElementsByClassName("close")[1];
+
+btn.onclick = function () {
+  abc.style.display = "block";
+};
+close.onclick = function () {
+  abc.style.display = "none";
+};
+close_footer.onclick = function () {
+  abc.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == abc) {
+    abc.style.display = "none";
+  }
+};
+
+var abc2 = document.getElementById("myModal2");
+function editct2(id){
+  abc2.style.display = "block";
+  document.getElementsByName("submit")[1].value=id;
+}
+function closemd(){
+  abc2.style.display="none";
+}
+function closemd2(){
+  abc.style.display="none";
+}
+
+//
+function editct(id){
+  abc.style.display = "block";
+  document.getElementsByName("submit")[0].value=id;
+}
+
+function editct3(id){
+  abc.style.display = "block";
+  document.getElementsByName("submit")[0].value=id;
+  var namect =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[0].innerText;
+  document.getElementById('ctname').value=namect;
+  var quoctich =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[2].innerText;
+  document.getElementById('nationality').value=quoctich;
+  var ngaysinh =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[3].innerText;
+  document.getElementById('birthday').value=ngaysinh;
+  var vitri =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[1].innerText;
+  document.getElementById('vitri').value=vitri;
+  var doi =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[5].innerText;
+  document.getElementById('idteam').value=doi;
+}
+
+function editct4(id){
+  abc2.style.display = "block";
+  document.getElementsByName("submit")[0].value=id;
+  var namect =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[0].innerText;
+  document.getElementById('ctname2').value=namect;
+  var quoctich =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[2].innerText;
+  document.getElementById('nationality2').value=quoctich;
+  var ngaysinh =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[3].innerText;
+  document.getElementById('birthday2').value=ngaysinh;
+  var vitri =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[1].innerText;
+  document.getElementById('vitri2').value=vitri;
+  var doi =document.getElementById(id).parentElement.parentElement.getElementsByTagName("td")[5].innerText;
+  document.getElementById('idteam2').value=doi;
+}
+//chinh sua ti so
+function undisableTxt(clickid) {
+  document.getElementById("homegoal"+clickid).disabled = false;
+  document.getElementById("awaygoal"+clickid).disabled = false;
+  document.getElementById("submit"+clickid).disabled = false;
+}
+
+//filter cau thu
+function myFunction() {
+  var input, filter, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  tbody = document.getElementById("tbodyct");
+  tr = tbody.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByClassName("tencauthu")[0];
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+  }
+}
+
+
+function selectmatch(id){
+  window.location="http://localhost/Test2/formmua.php?id="+ id;
+}
+
+function selectbaocao(id){
+  window.location="http://localhost/Test2/ADchitietbaocao.php?id="+ id;
+}
+
+function send(){
+    var arr =document.getElementsByTagName('input');
+    var teamname = arr[0].value;
+    var hlvname = arr[1].value;
+    var logo  =arr[2].value;
+    if(teamname==""||hlvname==""||logo==""){
+      alert ("Điền đầy đủ thông tin!");
+      return;
+    }
+    
+}
+
+
